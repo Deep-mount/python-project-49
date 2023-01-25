39958adebcf7db518ecf91f020abf79b75d91d09
Deep <you@example.com>
2023-01-25 21:27:32 +0300

    разработал brain-even

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,47 @@
+import prompt
+
+
+def welcome_user():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    return (name)
+
+
+number = [15, 6, 7]
+
+
+def even(x):
+    if (x % 2) == 0:
+        return 'yes'
+    elif (x % 2) == 1:
+        return 'no'
+
+
+def proverka(x, answer):
+    if even(x) == 'yes' and answer == 'yes':
+        return 'yes'
+    elif even(x) == 'no' and answer == 'no':
+        return 'no'
+    else:
+        return 'false'
+
+
+def main():
+    name = welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no"')
+
+    for k in number:
+        print(f'Quetion: {k}')
+        answer = prompt.string('Your answer: ')
+        if proverka(k, answer) == 'false':
+            return print(f'"{answer}" is wrong answer ;(.'
+                         f' Correct answer was "{even(k)}".')
+        else:
+            print('Correct!')
+
+    return print(f'Congratulations, {name}!')
+
+
+if __name__ == '__main__':
+    main()
