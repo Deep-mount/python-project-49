c407d194aee58dc0514b21fe4cc0c4b8c80a3f29
Deep <you@example.com>
2023-01-28 16:12:06 +0300

    Программа НОД работает

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ### Hexlet tests and linter status:
-[![Actions Status](https://github.com/Deep-mount/python-project-49/workflows/hexlet-check/badge.svg)](https://github.com/Deep-mount/python-project-49/actions)
+[![Actions Status](https://github.com/Deep-mount/pythk += 1on-project-49/workflows/hexlet-check/badge.svg)](https://github.com/Deep-mount/python-project-49/actions)
 
 https://asciinema.org/ - запись АКСИНЕМЫ - посмотреть позже
 sudo apt install asciinema
@@ -7,3 +7,4 @@ asciinema rec
 exit (Ctrl+D)
 https://asciinema.org/a/fs7C5wI5pohe2poLnlUEd1Q2c
 https://asciinema.org/a/jlIMMvsZtGZpUwDQ0WnxdkzMb
+https://asciinema.org/a/hVkY929FJTeaNRX5mpj6S7U6s
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,46 @@
+import prompt
+import random
+
+
+def welcome_user():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    return (name)
+
+
+def nod(n_1, n_2):
+    k = n_1
+    while k > 0:
+        if (n_2 % k == 0) and (n_1 % k == 0):
+            return k
+        k -= 1
+
+
+def main():
+    name = welcome_user()
+    lim_x = 20
+    lim_y = 20
+    k = 1
+    print('Find the greatest common divisor of given numbers.')
+
+    while k <= 5:
+        x = random.randint(1, lim_x)
+        y = random.randint(1, lim_y)
+        print(f'Quetion: {x} {y}')
+        answer = prompt.string('Your answer: ')
+        rez = nod(x, y)
+
+        if rez == int(answer):
+            print('Correct!')
+            k += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. "
+                  f"Correct answer was '{rez}'.")
+            return print(f"Let's try again, {name}!")
+
+    return print(f'Congratulations, {name}!')
+
+
+if __name__ == '__main__':
+    main()
